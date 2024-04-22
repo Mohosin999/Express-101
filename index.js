2566e4c31462651ca0489273e620572de003129d
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-04-22 11:01:21 +0600

    initial commit

--- index.js
+++ index.js
@@ -75,10 +75,3 @@ app.get("/htmlfile", (req, res) => {
 app.listen(4000, () => {
   console.log("Server is listening on PORT 4000");
 });
-
-// It could be an arrow function
-function handler(req, res, next) {
-  // read request objet
-  // process request
-  // response back the result
-}
